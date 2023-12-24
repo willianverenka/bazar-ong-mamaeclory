6853c6f9050d86a7afb113580be25310df0b1b4d
Will <willianverenkag@gmail.com>
2023-12-24 20:58:23 -0300

    update cors origin to frontend server

--- back/app.js
+++ back/app.js
@@ -7,14 +7,13 @@ const { ImgurClient } = require("imgur");
 const multer = require("multer");
 const helper = require("./helper");
 const bcrypt = require("bcrypt");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const fs = require("fs");
-const { FRONT_BASE_URL } = require('./constants')
 
 const corsOptions = {
-  origin: FRONT_BASE_URL,
-  credentials: true
-}
+  origin: "https://bazar-mamaeclory.vercel.app",
+  credentials: true,
+};
 
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
@@ -61,40 +60,49 @@ app.get("/api/posts", async (req, res) => {
   return res.send(posts);
 });
 
-app.post("/api/posts", helper.authenticateUser, multerUpload.single("imagem"), async (req, res) => {
-  if (!req.file) {
-    return res.status(400).send("Nenhuma imagem foi enviada.");
-  }
-  const imagem = req.file;
-  try {
-    const response = await client.upload({
-      image: fs.createReadStream(imagem.path),
-      type: "stream",
-    });
-    if (response.success) {
-      const imageUrl = response.data.link;
-      const post = new Post({
-        titulo: req.body.titulo,
-        valor: req.body.valor,
-        imagem_url: imageUrl,
-      });
-      await post.save();
-      helper.deletarImagem(imagem.path);
-      return res.status(201).send(post);
+app.post(
+  "/api/posts",
+  helper.authenticateUser,
+  multerUpload.single("imagem"),
+  async (req, res) => {
+    if (!req.file) {
+      return res.status(400).send("Nenhuma imagem foi enviada.");
     }
-    helper.deletarImagem(imagem.path);
-    return res.status(500).send(response.data);
-  } catch (error) {
-    return res.status(500).send(error);
+    const imagem = req.file;
+    try {
+      const response = await client.upload({
+        image: fs.createReadStream(imagem.path),
+        type: "stream",
+      });
+      if (response.success) {
+        const imageUrl = response.data.link;
+        const post = new Post({
+          titulo: req.body.titulo,
+          valor: req.body.valor,
+          imagem_url: imageUrl,
+        });
+        await post.save();
+        helper.deletarImagem(imagem.path);
+        return res.status(201).send(post);
+      }
+      helper.deletarImagem(imagem.path);
+      return res.status(500).send(response.data);
+    } catch (error) {
+      return res.status(500).send(error);
+    }
   }
-});
+);
 
 app.put("/api/posts/:id", helper.authenticateUser, async (req, res) => {
-  const post = await Post.findByIdAndUpdate(req.params.id, {
-    titulo: req.body.titulo,
-    valor: req.body.valor,
-  }, {new : true});
-  if(!post){
+  const post = await Post.findByIdAndUpdate(
+    req.params.id,
+    {
+      titulo: req.body.titulo,
+      valor: req.body.valor,
+    },
+    { new: true }
+  );
+  if (!post) {
     return res.status(404);
   }
   return res.status(200).send(post);
@@ -102,7 +110,7 @@ app.put("/api/posts/:id", helper.authenticateUser, async (req, res) => {
 
 app.delete("/api/posts/:id", helper.authenticateUser, async (req, res) => {
   const post = await Post.findByIdAndRemove(req.params.id);
-  if(!post){
+  if (!post) {
     return res.status(404).send();
   }
   return res.status(204).send();
@@ -121,21 +129,22 @@ app.post("/api/login", async (req, res) => {
     return res.status(401).send("Usuário inválido.");
   }
   const token = helper.generateToken();
-  return res.json({ token })
+  return res.json({ token });
 });
 
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
-  const existeUsuario = await Usuario.find({ username: `${username}` }).exec().length > 0;
+  const existeUsuario =
+    (await Usuario.find({ username: `${username}` }).exec().length) > 0;
   if (existeUsuario) {
-    return res.status(400).send("Usuário já existe.")
+    return res.status(400).send("Usuário já existe.");
   }
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = new Usuario({
       username: username,
-      passwordHash: hashedPassword
-    })
+      passwordHash: hashedPassword,
+    });
     user.save();
     return res.status(201).send(user);
   } catch (error) {
--- back/constants.js
+++
@@ -1,2 +0,0 @@
-module.exports.API_BASE_URL = process.env.API_BASE_URL;
-module.exports.FRONT_BASE_URL = process.env.FRONT_BASE_URL;
